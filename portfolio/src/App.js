d7a237653229300c07e428066ae37ef38b71138f
Victor Kamutu <victor.kamutu@strathmore.edu>
2026-06-18 15:35:22 +0300

    Build body component and wire it into App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/Header';
+import Body from './components/Body';
 import './App.css';
 
 const App = () => {
   return (
     <div id="app" className="App">
        <Header />
+       <Body />
     </div>
   );
 }
--- portfolio/src/components/Body.js
+++ portfolio/src/components/Body.js
@@ -1,1 +1,26 @@
 // Create your Body component here
+import { FaGithub, FaLinkedin, faLinkedin } from "react-icons/fa";
+import Avatar from "../assets/Software.png";
+
+const Body = () => {
+    return (
+        <div id="body" className="body">
+            <div className="body-container">
+                <div className="body-profile">
+                    <img className="body-img" alt='avatar' src={Avatar} />
+
+                    <div className="body-content">
+                        <div className="body-headline">Victor Muchina</div>
+                        <div className="body-text">Software Engineer</div>
+                    </div>
+                    <div className="body-icons">
+                        <a href="https://www.educate.io/" target="_blank" rel="noreferrer" className="icon-link"><i><FaGithub /></i></a>
+                        <a href="https://www.educate.io/" target="_blank" rel="noreferrer" className="icon-link"><i><FaLinkedin /></i></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Body;
