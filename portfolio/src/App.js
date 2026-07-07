8b0a049081bd314e795cc87573d18308c96da737
victormuchina <victor.kamutu@strathmore.edu>
2026-07-07 22:08:28 +0300

    Add Projects component with slider functionality and integrate into App.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,14 +1,16 @@
 import Header from './components/Header';
 import Body from './components/Body';
 import About from './components/About';
+import Projects from './components/Projects';
 import './App.css';
 
 const App = () => {
   return (
     <div id="app" className="App">
-       <Header />
-       <Body />
-       <About />
+      <Header />
+      <Body />
+      <About />
+      <Projects />
     </div>
   );
 }
