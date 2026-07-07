77826f24364a11046897772b72ded3a9812bc426
victormuchina <victor.kamutu@strathmore.edu>
2026-07-07 22:50:45 +0300

    Update Header and App styles: adjust header alignment, increase font size, and modify project section margin

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       </div>
       <div className="header-container">
         <div className="nav-item"><a href="#about">About</a></div>
-        <div className="nav-item"><a href="#Projects">Projects</a></div>
+        <div className="nav-item"><a href="#projects">Projects</a></div>
         <div className="nav-item"><a href="#skills">Skills</a></div>
       </div>
     </div>
