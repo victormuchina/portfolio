7df7895aa31bbb1f145753bd50994c28c2dd1d32
victormuchina <victor.kamutu@strathmore.edu>
2026-07-07 22:22:45 +0300

    Add Footer component with contact information and social links

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,6 +3,7 @@ import Body from './components/Body';
 import About from './components/About';
 import Projects from './components/Projects';
 import Skills from './components/Skills';
+import Footer from './components/Footer';
 import './App.css';
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <About />
       <Projects />
       <Skills />
+      <Footer />
     </div>
   );
 }
