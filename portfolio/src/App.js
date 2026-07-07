9bae7a97c4008231eccffe647f22eef8d7472c64
victormuchina <victor.kamutu@strathmore.edu>
2026-07-07 22:20:00 +0300

    Add Skills component and styles to display skill set

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import Body from './components/Body';
 import About from './components/About';
 import Projects from './components/Projects';
+import Skills from './components/Skills';
 import './App.css';
 
 const App = () => {
@@ -11,6 +12,7 @@ const App = () => {
       <Body />
       <About />
       <Projects />
+      <Skills />
     </div>
   );
 }
