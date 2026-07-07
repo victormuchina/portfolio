2fa1959b10c0c682479876331cae386ba4610f5e
victormuchina <victor.kamutu@strathmore.edu>
2026-07-07 23:08:55 +0300

    Update About component: replace image with personal photo, adjust image styles for better presentation

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,6 +1,5 @@
 // Create your About component here
-import IMG from "../assets/Detective.png";
-
+import IMG from "../assets/MyPhoto.jpg";
 const About = () => {
     return (
         <div id="about" className="about">
@@ -10,7 +9,7 @@ const About = () => {
                 <p className="about-desc">I am a software engineer with a passion for creating innovative solutions. With a strong background in web development and a keen eye for detail, I strive to build applications that are both functional and user-friendly. My experience spans across various technologies, and I am always eager to learn and adapt to new challenges.</p>
                 <div className="about-img">
                     <div className="about-img-wrapper">
-                        <img src={IMG} alt="Detective" />
+                        <img src={IMG} alt="Victor Muchina" />
                     </div>
                 </div>
             </div>
